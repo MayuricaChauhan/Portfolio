ad9d887b1322e29c164e82245f071faa269f29aa
MayuricaChauhan <iec@mayuricaeducation.in>
2024-11-04 13:51:16 +0530

    Update BlogDetail.jsx

--- src/components/Blogs/BlogDetail.jsx
+++ src/components/Blogs/BlogDetail.jsx
@@ -1,26 +1,26 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import BlogsData from './BlogData';
-import './BlogDetail.css'; // Import the CSS file
-
+import './BlogDetail.css';
 
 const BlogDetail = () => {
+  const { blogId } = useParams(); // Extract blogId from URL parameters
+  
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  
-  console.log('Blogs Data:', BlogsData);
-
   // Find the blog that matches the blogId
-  
+  const blog = BlogsData.find((b) => b.id === parseInt(blogId));
+
+  if (!blog) {
+    return <div>Blog not found</div>;
+  }
+
   return (
     <div className="blog-detail-container">
-      <h1 className="blog-detail-title">{BlogsData[0].title}</h1>
-      
-     
-      
-      <div dangerouslySetInnerHTML={{ __html: BlogsData[0].description }} className="blog-detail-content"></div>
+      <h1 className="blog-detail-title">{blog.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: blog.description }} className="blog-detail-content"></div>
     </div>
   );
 };
