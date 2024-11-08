7223b857abd74071300cfcb0db189bbf2f87d7ce
MayuricaChauhan <iec@mayuricaeducation.in>
2024-11-08 21:43:44 +0530

    Create Announcement.jsx

---
+++ src/components/Announcement/Announcement.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Announcement.css"
+const Announcement = () => {
+  return (
+    
+      <div className="banner">
+    <p>
+      <span className="emoji">🎓</span> Unlock Europe’s Educational Opportunities! <span className="emoji">🎓</span>
+      Curious about studying in Europe? Attend our seminar on November 20 and get insights into scholarships, career paths, and more. 
+      Your European dream is just a step away!
+    </p>
+ 
+    </div>
+  )
+}
+
+export default Announcement
