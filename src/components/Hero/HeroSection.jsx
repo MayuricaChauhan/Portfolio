c8316fc40ff9b62ae22ef3462ddf8f2d38d9d2b3
MayuricaChauhan <iec@mayuricaeducation.in>
2025-09-05 14:49:24 +0530

    Fix: Better error handling in brevo API route

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -7,10 +7,10 @@ const HeroSection = () => {
   useEffect(() => {
     AOS.init({
       duration: 1000, // Animation duration
-      once: false, 
+      once: false,
     });
   }, []);
- const handleLearnMoreClick = () => {
+  const handleLearnMoreClick = () => {
     document.getElementById("about")?.scrollIntoView({ behavior: "smooth" });
   };
   return (
@@ -19,35 +19,35 @@ const HeroSection = () => {
         className="bg-white shadow-lg rounded-lg p-6 max-w-xl mx-auto animate-fade-in"
         data-aos="fade-up" // Add animation type here
       >
-        <h1
+        <h1>Mayurica Education – Expert Study Abroad Counseling & Visa Guidance</h1>
+        <h2
           className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-1 flex items-center gap-2"
           data-aos="fade-up" // Add animation type here
           data-aos-delay="100" // Optional: delay before animation starts
         >
           Mayurica<span className="text-primary">Chauhan</span>
-        </h1>
+        </h2>
+
         <p
           className="text-sm text-gray-900 leading-relaxed mb-4 dm-serif-text-regular-italic"
           data-aos="fade-up" // Add animation type here
           data-aos-delay="300" // Optional: delay before animation starts
         >
-          With an impressive 8 years of experience in Human Resources and 6 years in international admissions, Ms. Mayurica Chauhan’s extensive journey as an educational counselor and her commitment to guiding students in their academic aspirations has been life-changing for many.
-          <br />
-          Ms. Mayurica’s educational background includes a Bachelor of Commerce from Kirori Mal College, Delhi and an MBA in Human Resource Management from the University of New Haven in the United States.
-          <br />
+          With an impressive 8 years of experience in Human Resources and 6 years in international student admissions, Ms. Mayurica Chauhan’s extensive journey as an educational counselor and study abroad consultant has been life-changing for many aspiring students. <br />
+          Ms. Mayurica’s educational background includes a Bachelor of Commerce from Kirori Mal College, Delhi, and an MBA in Human Resource Management from the University of New Haven, USA. <br />
           In addition, Ms. Chauhan holds the prestigious UCLA certification in career counselling.
-          <br />
-          She is also a proud recipient of the prestigious IEC certificate from University of California, Irvine. Ms. Chauhan’s own experience as an international student has allowed her to empathize deeply with aspiring students.
+          She is also a proud recipient of the IEC certificate from the University of California, Irvine. <br />
+          Ms. Chauhan’s own experience as an international student allows her to deeply empathize with and guide students who wish to study in USA, UK, Canada, Australia, and Europe.
         </p>
         <button
-              data-aos="fade-up"
-              data-aos-delay="500"
-              data-aos-offset="0"
-              className="primary-btn"
-              onClick={handleLearnMoreClick}
-            >
-              Learn More
-            </button>
+          data-aos="fade-up"
+          data-aos-delay="500"
+          data-aos-offset="0"
+          className="primary-btn"
+          onClick={handleLearnMoreClick}
+        >
+          Learn More
+        </button>
       </div>
     </div>
   );
